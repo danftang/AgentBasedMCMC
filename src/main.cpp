71efab4736b069d3f97a9292aa802f545ab6f735
Daniel Tang <from_github@freshjava.co.uk>
2022-04-25 18:04:55 +0100

    Clean up diagnostics

--- src/main.cpp
+++ src/main.cpp
@@ -5,21 +5,19 @@
 int main(int argc, char *argv[]) {
 
     /////////////// Generate data for figures 3 and 4 and statistics for table 2
-    FiguresForPaper<32,16>::generateStandardProblemFile(10.0);
-    FiguresForPaper<32,16>::generateStats(10000000);
+//    FiguresForPaper<32,16>::generateStandardProblemFile(10.0);
+//    FiguresForPaper<32,16>::generateStats(10000000);
     /////////////// Plot figures 3 and 4 and print statistics for table 2
     FiguresForPaper<32,16>::plotStats(false); // set to true to allow printing from plots.
 
-
     /////////////// Get timing for a sample on a single core
-    FiguresForPaper<32,16>::sampleTiming(1000000);
-
+//    FiguresForPaper<32,16>::sampleTiming(1000000);
 
     ////////////// Test MCMC against small, tractable examples
-    Experiments::CatMouseAssimilation();
-    Experiments::PredPreySingleObservation();
-    Experiments::CatMouseSingleObservation();
-    Experiments::BinomialAgentSingleObservation();
+//    Experiments::CatMouseAssimilation();
+//    Experiments::PredPreySingleObservation();
+//    Experiments::CatMouseSingleObservation();
+//    Experiments::BinomialAgentSingleObservation();
 
     return 0;
 }
